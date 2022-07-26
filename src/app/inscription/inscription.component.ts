d9d24913f9f87fbdadd394f35fc586f97acf4dcc
fbansept <bansept.franck@gmail.com>
2022-07-26 12:00:15 +0200

    appel post utilisateur

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,6 +9,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class InscriptionComponent implements OnInit {
 
+  public avatar: File | null = null
+  public sourceAvatar: string = ""
+
   public formulaire: FormGroup = new FormGroup(
     {
       email: new FormControl("", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
@@ -15,16 +19,35 @@ export class InscriptionComponent implements OnInit {
     }
   );
 
-  constructor() { }
+  constructor(private client : HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onChangeAvatar(e: any) {
+    if (e.target.files[0]) {
+      this.avatar = e.target.files[0]
+
+      const reader = new FileReader()
+
+      reader.onload = (eventReader: any) => this.sourceAvatar = eventReader.target.result
+
+      reader.readAsDataURL(e.target.files[0])
+    }
   }
 
   onSubmit() {
     this.formulaire.markAllAsTouched();
 
-    if(this.formulaire.valid) {
-      
+    
+
+    if (this.formulaire.valid) {
+
+      const headers = new HttpHeaders()
+        .set('content-type', 'application/json')
+        .set('Access-Control-Allow-Origin', '*');
+
+        this.client.post("http://localhost:4000/utilisateur", this.formulaire.value, {headers})
     }
 
   }
